1d356dd8af3bbf77bd0998bfcaf014ae1fb237aa
TrevorK-dev <trevor.knisley01@gmail.com>
2020-05-09 14:12:06 -0700

    Updated

--- PythonDrills/PythonDrill.py
+++ PythonDrills/PythonDrill.py
@@ -1,11 +1,28 @@
 import os, sys
-dirs = os.listdir( "C:\A" )
+path = "C:\A"
+dirs = os.listdir( path )
+print(dirs)
 
-print( "C:\A" )
+for x in dirs:
+##    abspath = os.path.join(path, x)
+##    print(x)
+##    print(abspath)
+##    gmtime = os.path.getmtime( abspath )
+##    print(gmtime)
+    if x.endswith(".txt"):
+        abspath = os.path.join(path, x)
+        print(abspath)
+        gmtime = os.path.getmtime( abspath )
+        print(gmtime)
+    
 
-abspath = os.path.join("C:\A","Hello.txt")
 
-print(abspath)
+##print(abspath)
 
 
-os.path.getmtime( "C:\A" )
+##os.path.getmtime( "C:\A" )
+
+##os.path.join("C:\A", "Hello.txt")
+
+##for os.listdir( path ) = ".txt":
+    
